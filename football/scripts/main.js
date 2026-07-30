c552768b2b91714ffaa46ec833b23fd7ed65a965
jon-tj <jh.tjemsland@gmail.com>
2026-07-31 00:08:51 +0200

    check home advantage

--- football/scripts/main.js
+++ football/scripts/main.js
@@ -11,7 +11,7 @@ function setTableValues(valuesMatrix = null) {
         const i = tableSize - 1 - k; // Reverse the order of rows
         for (let j = 0; j < tableSize; j++) {
             const cell = document.createElement('td');
-            const prefix = i > j ? "A" : (i == j ? "" : "B");
+            const prefix = i > j ? "Home" : (i == j ? "" : "Away");
             const prob = valuesMatrix ? valuesMatrix[i][j] : 0;
             let hue = "blues-" + Math.min(9, Math.floor(prob * 200));
             cell.innerHTML = `<p>${i + 1}-${j + 1} ${prefix}</p><p class='muted'>${(prob * 100).toFixed(2)}%</p>`;
@@ -37,11 +37,13 @@ btnCalculate.addEventListener('click', (e) => {
     const oddsDraw = readNumber('odds-draw', 1);
     const oddsTeamA = readNumber('odds-team-a', 1);
     const oddsTeamB = readNumber('odds-team-b', 1);
+    const homeAdvantage = document.getElementById('home-advantage').checked;
 
     const resultMatrix = [];
-    const xGA = 2 + (eloTeamB - eloTeamA) / 400 + (1 / oddsDraw) * 0.5;
-    console.log(eloTeamA ?? 0, eloTeamB, oddsDraw, xGA);
-    const xGB = 2 + (eloTeamA - eloTeamB) / 400 + (1 / oddsDraw) * 0.5;
+    const priorHome = homeAdvantage ? 1.75 : (1.75 + 1.18) / 2;
+    const priorAway = homeAdvantage ? 1.18 : (1.75 + 1.18) / 2;
+    const xGA = priorHome + (eloTeamB - eloTeamA) / 400 + (1 / oddsDraw) * 0.5;
+    const xGB = priorAway + (eloTeamA - eloTeamB) / 400 + (1 / oddsDraw) * 0.5;
 
     for (let i = 0; i < tableSize; i++) {
         const row = [];
